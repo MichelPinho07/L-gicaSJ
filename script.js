0b19ac92e34fa19e051043fbf29194a9c8ae9e1a
Michel de Pinho <166452536+MichelPinho07@users.noreply.github.com>
2024-04-08 21:59:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let resultado = document.getElementById("divResultado")
+// console.log(resultado);
+
+
+// // resultado.innerHTML = "Ola"
+
+
+// let contador = 0
+// while(contador < 10){
+//     // console.log(contador);
+//     resultado.innerHTML += contador + " patinho foi passear...<br>"
+//     contador++
+// }
+
+// let cont = 0
+// while(cont <= 10){
+//     let quadrado = cont **2
+//     resultado.innerHTML += quadrado + "<br>"
+//     cont++    
+
+// }
+
+// let cont = 10
+// while(cont > 0){
+//     resultado.innerHTML += cont + "<br>"
+//     cont--
+// }
+
+// let cont = 0, soma = 0
+// while(cont <= 100){
+//     soma = cont + soma
+//     resultado.innerHTML += soma + "<br>"
+//     cont++
+// }
+
+// let cont = 0, tabuada
+// let numero = Number(prompt("Digite um numero: "))
+// while(cont <= 10){
+//     tabuada = cont * numero
+//     resultado.innerHTML += tabuada + "<br>"
+//     cont++
+// }
+
+
